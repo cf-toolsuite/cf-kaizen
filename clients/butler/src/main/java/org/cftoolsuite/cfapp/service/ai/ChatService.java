190004561a20950e3a4e1a2b176bec43cd80d859
Chris Phillipson <cphillipson@pivotal.io>
2025-03-10 17:42:31 -0700

    Update how MCP client interacts with MCP server and LLM.  On each request ask provider for toolCallbacks.

--- clients/butler/src/main/java/org/cftoolsuite/cfapp/service/ai/ChatService.java
+++ clients/butler/src/main/java/org/cftoolsuite/cfapp/service/ai/ChatService.java
@@ -1,13 +1,16 @@
 package org.cftoolsuite.cfapp.service.ai;
 
+import io.modelcontextprotocol.client.McpAsyncClient;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.mcp.AsyncMcpToolCallbackProvider;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
@@ -15,17 +18,22 @@ import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvis
 public class ChatService {
 
     private final ChatClient chatClient;
+    private final List<McpAsyncClient> mcpAsyncClients;
 
-    public ChatService(ChatModel chatModel, ChatMemory chatMemory, ToolCallbackProvider provider) {
+    public ChatService(
+            ChatModel chatModel, ChatMemory chatMemory,
+            List<McpAsyncClient> mcpAsyncClients) {
         this.chatClient = ChatClient.builder(chatModel)
-                .defaultTools(provider)
                 .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory), new SimpleLoggerAdvisor())
                 .defaultAdvisors(a -> a.param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
                 .build();
+        this.mcpAsyncClients = mcpAsyncClients;
     }
 
     public Flux<String> streamResponseToQuestion(String question) {
+        AsyncMcpToolCallbackProvider provider = new AsyncMcpToolCallbackProvider(mcpAsyncClients);
         return constructRequest(question)
+                .tools(provider.getToolCallbacks())
                 .stream()
                 .content();
     }
@@ -34,12 +42,6 @@ public class ChatService {
         return chatClient
                 .prompt()
                 .user(question);
-    }
-
-    public Flux<String> chat(String question) {
-        return constructRequest(question)
-                .stream()
-                .content();
     }
 
 }
