31a5cfa1775c9d06bdbd7e11088179f0b643b885
Chris Phillipson <cphillipson@pivotal.io>
2025-03-12 18:21:15 -0700

    Update OpenAI-compatible chat support

---
+++ clients/butler/src/main/java/org/cftoolsuite/cfapp/config/ChatSupplemental.java
@@ -0,0 +1,16 @@
+package org.cftoolsuite.cfapp.config;
+
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatSupplemental {
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
+}
