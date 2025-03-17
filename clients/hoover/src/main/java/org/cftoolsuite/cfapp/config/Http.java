66fac58f3aec4afcebbbc7a56b3e75a41bad4118
Chris Phillipson <cphillipson@pivotal.io>
2025-03-17 11:18:03 -0700

    Simplify OpenAi chat configuration. Rely on OpenAiAutoConfiguration entirely.  Add accept-encoding header to RestClientCustomizer.

--- clients/hoover/src/main/java/org/cftoolsuite/cfapp/config/Http.java
+++ clients/hoover/src/main/java/org/cftoolsuite/cfapp/config/Http.java
@@ -3,6 +3,7 @@ package org.cftoolsuite.cfapp.config;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 
 import java.time.Duration;
@@ -16,6 +17,7 @@ public class Http {
             SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
             factory.setConnectTimeout((int) Duration.ofMinutes(10).toMillis());
             factory.setReadTimeout((int) Duration.ofMinutes(10).toMillis());
+            restClientBuilder.defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate");
             restClientBuilder.requestFactory(factory);
         };
     }
